0cc4cfac179e75fdfa349dc9dc3fc9b2541dde83
Meir <meir@asininsight.com>
2026-03-26 20:25:33 -0700

    auto-load demo on entry, fix refresh caching issue

--- server.py
+++ server.py
@@ -26,6 +26,8 @@ def add_security_headers(response):
     response.headers["X-Content-Type-Options"] = "nosniff"
     response.headers["X-Frame-Options"] = "SAMEORIGIN"
     response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin"
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
     return response
 
 @app.errorhandler(404)
